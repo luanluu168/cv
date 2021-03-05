cdee44cf871768a93eb6089b91fce462fff6c88e
LuanLuu <lluu3@mail.sfsu.edu>
2021-03-04 19:04:39 -0800

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,6 +129,13 @@
     delay: 10,
     time: 1000
   });
+  jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+  });
+  
 
   // Skills section
   $('.skills-content').waypoint(function() {
